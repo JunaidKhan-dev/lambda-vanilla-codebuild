04e8f96c8aca4864ac67bfba7fc0979d945e89b1
EC2 Default User <ec2-user@ip-172-31-8-242.eu-west-1.compute.internal>
2020-12-27 12:59:19 +0000

    update1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 exports.handler = async function(event, context) {
     const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    console.log('lambda working...')
 
     return response.data
 }
